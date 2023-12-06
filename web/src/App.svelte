6048b06a1e520ba3975702ed9d3a5efb6636787c
Berk Ormanlı <berkormanl@gmail.com>
2023-12-06 17:52:19 +0300

    Initial commit

---
+++ web/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import VisibilityProvider from './providers/VisibilityProvider.svelte';
+  import HelloWorld from './components/ExampleComponent.svelte';
+  import { debugData } from './utils/debugData';
+
+  debugData([
+    {
+      action: 'setVisible',
+      data: true,
+    },
+  ]);
+</script>
+
+<main>
+  <VisibilityProvider>
+    <HelloWorld />
+  </VisibilityProvider>
+</main>
